0939034480f4b9b9c77bf733933853ad038470ab
Mihir Dadhich <mihir2029dadhich@gmail.com>
2024-04-02 16:43:43 +0530

    color and nav comp.

---
+++ src/assets/theme-dark/components/card/cardMedia.js
@@ -0,0 +1,24 @@
+// .
+
+// Crypto Dash React Base Styles
+import borders from "assets/theme-dark/base/borders";
+
+// Crypto Dash React Helper Functions
+import pxToRem from "assets/theme-dark/functions/pxToRem";
+
+const { borderRadius } = borders;
+
+const cardMedia = {
+  styleOverrides: {
+    root: {
+      borderRadius: borderRadius.xl,
+      margin: `${pxToRem(16)} ${pxToRem(16)} 0`,
+    },
+
+    media: {
+      width: "auto",
+    },
+  },
+};
+
+export default cardMedia;
---
+++ src/assets/theme-dark/components/iconButton.js
@@ -0,0 +1,18 @@
+// .
+
+// Crypto Dash React Base Styles
+import colors from "assets/theme-dark/base/colors";
+
+const { transparent } = colors;
+
+const iconButton = {
+  styleOverrides: {
+    root: {
+      "&:hover": {
+        backgroundColor: transparent.main,
+      },
+    },
+  },
+};
+
+export default iconButton;
